4e1b079aa2aa2ca6a93225ad779025164a6f0c78
Johnny Mace <Johnny Mace>
2016-12-08 17:08:03 +0100

    entity relation in progress

--- src/EHSBundle/Form/UserType.php
+++ src/EHSBundle/Form/UserType.php
@@ -3,6 +3,11 @@
 namespace EHSBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +18,17 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('email')->add('lastname')->add('firstname')->add('address')->add('zipCode')->add('city')->add('phone')->add('birth')->add('newsletter')        ;
+        $builder
+            ->add('email', EmailType::class, array('label'=> 'Adresse e-mail'))
+            ->add('lastname', TextType::class, array('label' => 'Nom ou Raison Sociale'))
+            ->add('firstname' ,TextType::class, array('label' => 'Prénom (Facultatif si Raison Sociale'))
+            ->add('address', TextType::class, array('label' => 'Adresse' ))
+            ->add('zipCode', IntegerType::class, array('label' => 'Code Postal'))
+            ->add('city', TextType::class, array('label' => 'Ville'))
+            ->add('phone' , TextType::class, array('label' => 'Téléphone'))
+            ->add('birth', BirthdayType::class, array('format'=> 'dd-MM-yyyy','label' => 'Date de naissance'))
+            ->add('newsletter', CheckboxType::class, array('label'=> 'recevoir la newsLetter? '))
+        ;
     }
     
     /**
